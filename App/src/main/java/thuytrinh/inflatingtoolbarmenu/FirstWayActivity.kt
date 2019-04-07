db0f1a5889ad47df2ccfc84f047421caec39f3ce
Thuy Trinh <thuy.trinh@cobi.bike>
2019-04-07 23:41:59 +0200

    Change prj structure

--- App/src/main/java/thuytrinh/inflatingtoolbarmenu/FirstWayActivity.kt
+++ App/src/main/java/thuytrinh/inflatingtoolbarmenu/FirstWayActivity.kt
@@ -1,11 +1,11 @@
-package thuytrinh.inflatingtoolbarmenu.app2
+package thuytrinh.inflatingtoolbarmenu
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class App2Activity : AppCompatActivity() {
+class FirstWayActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.app2)
+    setContentView(R.layout.main)
   }
 }
