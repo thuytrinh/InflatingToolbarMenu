bf5660ffbe8e9da5db482b8070fdd3294cb5cba1
Thuy Trinh <thuy.trinh@cobi.bike>
2019-04-07 18:55:44 +0200

    Rename

--- App1/src/main/java/thuytrinh/inflatingtoolbarmenu/app1/App1Activity.kt
+++ App1/src/main/java/thuytrinh/inflatingtoolbarmenu/app1/App1Activity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class App1Activity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_app1)
+    setContentView(R.layout.app1)
   }
 }
--- App2/src/main/java/thuytrinh/inflatingtoolbarmenu/app2/App2Activity.kt
+++ App2/src/main/java/thuytrinh/inflatingtoolbarmenu/app2/App2Activity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class App2Activity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_app2)
+    setContentView(R.layout.app2)
   }
 }
